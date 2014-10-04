eb1ae80939893dd971851168ba90ee4d1ace6d8e
ldanieluncor <ldaniel@uncor.net>
2014-10-04 00:31:43 -0300

    Hello Moon App
    From coderanch's Android book

---
+++ src/com/example/hellomoon/AudioPlayer.java
@@ -0,0 +1,38 @@
+package com.example.hellomoon;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+
+public class AudioPlayer {
+
+	private MediaPlayer mediaPlayer;
+
+	public void stop() {
+		if (mediaPlayer != null) {
+			mediaPlayer.release();
+			mediaPlayer = null;
+
+		}
+	}
+
+	public void play(Context context) {
+		stop();
+		mediaPlayer = MediaPlayer.create(context, R.raw.one_small_step);
+		mediaPlayer.setOnCompletionListener(onCompletionListener());
+		mediaPlayer.start();
+	}
+
+	private OnCompletionListener onCompletionListener() {
+		MediaPlayer.OnCompletionListener completionListener = new MediaPlayer.OnCompletionListener() {
+
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				stop();
+
+			}
+		};
+		return completionListener;
+	}
+
+}
